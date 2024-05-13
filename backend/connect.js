2712d4f8fcb60f70a9ce9cfeaa43e9422d3974bc
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2024-05-13 23:24:26 +0300

    hided the database connection information

--- backend/connect.js
+++ backend/connect.js
@@ -1,8 +1,10 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const db = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "admin",
-	database: "social",
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DB_NAME,
 });
