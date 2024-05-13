a2558c05feba8db3668748d2eb007470ec755442
Mohamed Abdelfattah <mhabdelfattah22@gmail.com>
2024-05-13 23:57:35 +0300

    update environment variable names in DB connection

--- backend/connect.js
+++ backend/connect.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createConnection({
-	host: process.env.HOST,
-	user: process.env.USER,
-	password: process.env.PASSWORD,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 });
